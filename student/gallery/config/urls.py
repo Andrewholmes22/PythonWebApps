20efdce0859628b3b436fc3e7373015fd842ac79
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2023-09-15 22:59:41 -0600

    project 4 save

--- student/gallery/config/urls.py
+++ student/gallery/config/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from photos.views import PhotoView
 urlpatterns = [
     path('<str:name>', PhotoView.as_view()),
+    path('',PhotoListView.as_view())
 ]
--- student/gallery/photos/views.py
+++ student/gallery/photos/views.py
@@ -8,4 +8,13 @@ class PhotoView(TemplateView):
         name=kwargs['name']
         image= f'static/images/{name}'
         return {'photo':image}
-    
+
+    def photo_list():
+        photos = Path('static/images').iterdir()
+        photos = [ f for i, f in photos]
+        return photos
+class PhotoListView(TemplateView):
+    template_name = 'photos.html'
+
+    def get_context_data(self, **kwargs):
+        return dict(photos=photo_list())
