e7b252c8446f53072190c10f927b67a5fca2f652
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2023-10-23 16:34:08 -0600

    5

--- student/05/Superhero/config/urls.py
+++ student/05/Superhero/config/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.admin import site
 from django.urls import path
 from django.urls.conf import include
-
+from django.contrib import admin
 
 urlpatterns = [
-    path('', include('hero.urls')),
+    path(r'admin/', site.urls),
 ]
