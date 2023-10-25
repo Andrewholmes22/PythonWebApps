75ef39ef5d3e5b304247f8aa22e5b2dd0e5f1046
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2023-10-25 15:36:58 -0600

    6 test 3

--- student/06/Superhero/hero/models.py
+++ student/06/Superhero/hero/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Superhero(models.Model):
     name = models.CharField(max_length=100)
-    identity = models.CharField(max_length=100)
+    identity = models.CharField(max_length=100,blank=True)
     description = models.TextField()
-    strength = models.CharField(max_length=100)
-    weakness = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    strength = models.CharField(max_length=100,blank=True)
+    weakness = models.CharField(max_length=100,blank=True)
+    image = models.CharField(max_length=100,blank=True)
 
     def __str__(self):
         return self.name
